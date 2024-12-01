0b8e38e544b2a441637d426051e54533aee3d7dc
Nelson-wedefineapps <nelson@wedefineapps.com>
2024-12-01 16:48:17 +0530

    ngnx config change vite config change

--- frontEnd/vite.config.ts
+++ frontEnd/vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth-api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace('/auth-api', ''),
+        secure: false,
+      },
+      '/post-api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace('/post-api', ''),
+        secure: false,
+      },
+    },
+  }
 })

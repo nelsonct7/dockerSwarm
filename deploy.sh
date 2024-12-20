32bf501e262efd6f82f1bb5eba39874ca3e198af
Nelson-wedefineapps <nelson@wedefineapps.com>
2024-12-20 18:17:44 +0530

    test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-EC2_HOST="ubuntu@ec2-98-84-133-3.compute-1.amazonaws.com"
-KEY_PATH="../cred/ThreeTierPem.pem"
+EC2_HOST="ubuntu@ec2-52-90-115-184.compute-1.amazonaws.com"
+KEY_PATH="../cred/newTest.pem"
 
 # Copy the docker-swarm.yaml to EC2
 scp -i $KEY_PATH docker-swarm.yaml $EC2_HOST:~/docker-swarm.yaml
